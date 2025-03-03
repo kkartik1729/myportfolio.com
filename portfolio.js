7876f8eb423f85b2d74cbcb180caecf7a785496a
Kartik Hande <handekartik181@gmail.com>
2025-03-03 22:46:49 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer"], // Fixed typo in "Frontend"
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true,
+});
+
